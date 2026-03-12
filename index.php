201c670b8bc94c80b857c802affc6c14c6ccb1e4
Julibe <mail@julibe.com>
2026-03-11 20:04:08 -0500

    Cross origin

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
+
 	/*
 		* ----------------------------------------------------------------------------
 		* Omnex - The Asset Explorer where everything is accounted for
